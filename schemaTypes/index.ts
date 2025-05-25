ee94c750ff8e1dc7263410bbc40a565bdbb33f39
FictionCode00 <sumitkumar19016@gmail.com>
2025-05-25 17:21:42 +0530

    latestMedia added

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -3,5 +3,6 @@ import country from './country'
 import news from './news'
 import user from './user'
 import ad from './ad'
+import latestMedia from './latestMedia'
 
-export const schemaTypes = [category, country, news, user, ad]
+export const schemaTypes = [category, country, news, user, ad,latestMedia]
